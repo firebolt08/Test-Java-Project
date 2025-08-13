b3c4251358e77856b7d58e01be919d08c2c69b67
firebolt08 <zorzac2001@gmail.com>
2025-08-12 22:33:25 -0500

    Added first files.

---
+++ src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int a = 5;
+        String name = "Susan";
+        name.toLowerCase();
+        System.out.println(name);
+    }
+}
